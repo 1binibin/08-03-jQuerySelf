ce78bc908868034b8bf83b47c4a0f585f05b5af0
1binibin <namwonbin@gmail.com>
2021-08-03 01:03:30 +0900

    first commit

---
+++ js/03.show.js
@@ -0,0 +1,36 @@
+$('.bt1').on('click', function() {
+    // 시간을 안주면 바로 없어짐(display:none;)
+    // 시간을 주면 주어진 시간동안 width, height, opacity가 줄어들면서
+    //width, height, opacity가 0이되면 사라짐 (display:none)
+    $('.box').hide(1000); 
+});
+
+$('.bt2').on('click', function() {
+    $('.box').stop().show(1000); 
+});
+
+$('.bt3').on('click', function() {
+    $('.box').stop().fadeOut('slow');    // "slow"(600), "normal"(400), "fast"(200), ms
+});
+
+$('.bt4').on('click', function() {
+    $('.box').stop().fadeIn('fast'); 
+});
+
+$('.bt5').on('click', function() {
+    $('.box').stop().slideUp(1000); 
+});
+
+$('.bt6').on('click', function() {
+    $('.box').stop().slideDown(1000); 
+});
+
+
+$('.header-wrapper .navi').on('mouseenter', function () {
+    //$(this).childern('.sub-wp').stop().slideDown(1000);   // 자식 선택자
+    $(this).find('.sub-wp').stop().slideDown(200); // 자손 선택자
+});
+
+$('.header-wrapper .navi').on('mouseleave', function () {
+    $(this).find('.sub-wp').stop().slideUp(200);
+})
---
+++ js/05.gallery..js
@@ -0,0 +1,111 @@
+// window (jQuery), Math, String, Number, Date, Array, Console, JSON, Object
+
+var products = [
+	{
+		name: 'Good Product1',
+		src: '../img/p1.jpg',
+		price: 12.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product2',
+		src: '../img/p2.jpg',
+		price: 13.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product3',
+		src: '../img/p3.jpg',
+		price: 13.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product4',
+		src: '../img/p4.jpg',
+		price: 14.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product5',
+		src: '../img/p5.jpg',
+		price: 15.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product6',
+		src: '../img/p6.jpg',
+		price: 16.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product7',
+		src: '../img/p7.jpg',
+		price: 17.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product8',
+		src: '../img/p8.jpg',
+		price: 18.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product9',
+		src: '../img/p9.jpg',
+		price: 19.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product10',
+		src: '../img/p10.jpg',
+		price: 120.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product11',
+		src: '../img/p11.jpg',
+		price: 121.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, {
+		name: 'Good Product12',
+		src: '../img/p12.jpg',
+		price: 122.99,
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium, eum earum mollitia neque, vel est itaque placeat laborum distinctio ex ab expedita sapiente ea unde atque, nesciunt sed praesentium!',
+	}, 
+];
+
+$(document).ready(function() {
+
+
+	for(var i=0, html; i<products.length; i++) {
+		html  = '<li class="prd">';
+		html += '	<div class="imgs">'; 
+		html += '		<img src="'+products[i].src+'" class="w100">';
+		html += '	</div>';
+		html += '	<div class="conts">';
+		html += '		<h2 class="name">'+products[i].name+'</h2>';
+		html += '		<p class="content">'+products[i].content+'</p>';
+		html += '		<div class="price">$'+products[i].price+'</div>';
+		html += '	</div>';
+		html += '</li>';
+		$(html).appendTo('.prd-wrap').find('img').click(function() {
+			var src = $(this).attr('src');
+			$('.modal-wrap img').attr('src',src);
+			$('.modal-wrapper').stop().fadeIn(800);
+		});
+	}
+	/*
+	// .prd 생성 완료
+	$('.prd img').click(function() {
+		var src = $(this).attr('src'); // this.src ; Getter
+		$('.modal-wrap img').attr('src',src); // this.src = src; Setter
+		//$('.modal-wrapper').css('display', 'flex'); // this.style.display = 'flex'; Setter
+		//$('.modal-wrapper').css('display'); // this.style.display; Getter
+		$('.modal-wrapper').stop().fadeIn(800);
+	})
+	*/
+	$('.modal-wrapper .bt-close').click(function() {
+		//$('.modal-wrapper').css('display', 'none');
+		$('.modal-wrapper').stop().fadeOut(800);
+	});
+});
+
+// document.getElementsByClassName('prd-wrap')[0].innerHTML += html;
+// $('.prd-wrap')[0].innerHTML += html;
+// $('.prd-wrap').html(); // $('.prd-wrap')[0].innerHTML; (Getter)
+// $('.prd-wrap').html(html); // $('.prd-wrap')[0].innerHTML = html; (Setter)
+// $('.prd-wrap').html( $('.prd-wrap').html() + html ); // $('.prd-wrap')[0].innerHTML += html;
+// $('.prd-wrap').append(html); // $('.prd-wrap')[0].innerHTML += html;
+// var modal = document.getElementsByClassName('modal-wrapper')[0];
+// var img = modal.getElementsByTagName('img')[0];
+// $('.modal-wrapper img')
