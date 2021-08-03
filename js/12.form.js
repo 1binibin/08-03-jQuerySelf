9d7c077aa5c7bf579628d1acfeffb6451fe5c9ce
1binibin <namwonbin@gmail.com>
2021-08-03 23:16:11 +0900

    21-08-03 복습!

--- js/12.form.js
+++ js/12.form.js
@@ -1,30 +1,35 @@
 /*************** Global init ****************/
 var scores = [
-	{ id: 1, name: '홍길동', kor: 95, eng: 80, math: 75 },
-	{ id: 2, name: '홍길순', kor: 88, eng: 95, math: 85 },
-	{ id: 3, name: '홍길만', kor: 90, eng: 85, math: 85 },
-]
+	{id:1, name: '홍길동', kor: 95, eng:80, math:75},
+	{id:2, name: '홍길순', kor: 88, eng:95, math:85},
+	{id:3, name: '홍길만', kor: 90, eng:85, math:85}
+];
 
 /*************** Function init ****************/
 function init() {
-	// for(var i=0; i<scores.length; i++) { createRow(scores[i]) }
-	for(var i in scores) { createRow(scores[i]) }
-	// for(var v of scores) { createRow(v) } // IE11 미지원
-	// scores.forEach(function(v, i) { createRow(v) })
+	//for(var i=0; i<scores.length; i++) {createRow(scores[i]);}
+	for(var i=0 in scores) { 	createRow(scores[i]) };
+	//for(var v of scores) { createRow(v)} //IE11 미지원
+	//scores.forEach(function(v, i) { createRow(v)})
 }
 
 function average() {
-	for(var i=0, sum=0; i<arguments.length; i++) {
+	for(var i=0, sum=0; i<arguments.length; i++){
 		sum += Number(arguments[i]);
 	}
 	return (sum/arguments.length).toFixed(2);
 }
 
+
 function total() { // 가변인자
-	for(var i=0, sum=0; i<arguments.length; i++) {
+	for(var i=0, sum=0; i<arguments.length; i++){
 		sum += Number(arguments[i]);
 	}
 	return sum;
+}
+
+function createTag() {
+	
 }
 
 function createRow(data) {
@@ -43,60 +48,45 @@ function createRow(data) {
 	html += '<td>'+math+'</td>';
 	html += '<td>'+tot+'</td>';
 	html += '<td>'+avg+'</td>';
-	html += '</tr>';
+	html += '</td>'
 	$(html).prependTo('.score-tbody');
 }
 
-
 /*************** Event callback ****************/
 function onShowForm() {
-	$('.score-form').stop().slideDown(300, function() {
+	$('.score-form').stop().slideDown(300, function () {
 		document.scoreForm.name.focus();
 	});
 }
-
-function onSubmit() { // 검증(Validation)
+function onSubmit() { // 검증
 	var f = document.scoreForm;
 	var name = f.name.value.trim();
 	var kor = f.kor.value;
 	var eng = f.eng.value;
 	var math = f.math.value;
-	if(name.length < 2) {
-		alert('이름을 입력하세요.');
+	if(name.length < 2 ){
+		alert('이름을 입력하세요.')
 		f.name.focus();
 		return false;
 	}
-	if(kor < 0 || kor > 100 ) {
-		alert('점수가 올바르지 않습니다.');
+	if (kor < 0 ||kor >100) {
+		alert('점수가 올바르지 않습니다.')
 		f.kor.focus();
 		return false;
 	}
-	if(eng < 0 || eng > 100 ) {
-		alert('점수가 올바르지 않습니다.');
+	if (eng < 0 ||eng >100) {
+		alert('점수가 올바르지 않습니다.')
 		f.eng.focus();
 		return false;
 	}
-	if(math < 0 || math > 100 ) {
-		alert('점수가 올바르지 않습니다.');
+	if (math < 0 ||math >100) {
+		alert('점수가 올바르지 않습니다.')
 		f.math.focus();
 		return false;
 	}
-    scores.push({
-        id:scores.length +1, 
-        name:name, 
-        kor:kor,
-        eng:eng,
-        math:math
-        });
-        createRow( scores[scores.length -1] );   // 마지막 요소의 값
-        f.reset();
-        f.name.focus();
-	return false;
+	
 }
-
-
 /*************** Event init ****************/
 $('.bt-show').click(onShowForm);
 
 init();
-
