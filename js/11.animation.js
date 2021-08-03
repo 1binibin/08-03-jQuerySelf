9ff46caa45b5351d204c7fdac87c23c88b604317
1binibin <namwonbin@gmail.com>
2021-08-03 22:30:34 +0900

    animation

--- js/11.animation.js
+++ js/11.animation.js
@@ -18,14 +18,13 @@ var interval;
 /***************** 이벤트콜백 ******************/
 function onFadeIn(_el) {
 	var el = document.querySelector(_el);
-	var opacity = getComputedStyle(el).getPropertyValue('opacity'); // String
+	var opacity = getComputedStyle(el).getPropertyValue('opacity'); //string
 	el.style.opacity = Number(opacity) + 0.01;
-	if(el.style.opacity >= 0.99) {
+	if(el.style.opacity >= 0.9) {
 		el.style.opacity = 1;
 		clearInterval(interval);
 	}
 }
-
 function onFadeOut(_el) {
 	var el = document.querySelector(_el);
 	var opacity = getComputedStyle(el).getPropertyValue('opacity');
@@ -35,41 +34,37 @@ function onFadeOut(_el) {
 		clearInterval(interval);
 	}
 }
-
 function onMoving(_el) {
 	var el = document.querySelector(_el);
-	var left = Number(getComputedStyle(el).getPropertyValue('left').replace('px', ''));
+	var left = Number(getComputedStyle(el).getPropertyValue('left').replace('px',''));
 	var target = el.parentNode.clientWidth - el.clientWidth;
-	left += (target - left) * 0.025;
+	left += (target-left) * 0.025;
 	el.style.left = left + 'px';
-	if(left > target - 1) {
+	if(left >= target - 1) {
 		el.style.left = target + 'px';
 		clearInterval(interval);
 	}
-	/* el.style.left = (left + 2) + 'px';
-	if(left + 1 >= target) {
+	/* el.style.left = left + 1 + 'px';
+	if(left +1 >= target) {
 		el.style.left = target + 'px';
-		clearInterval(interval);
 	} */
 }
 
 function onShow() {
 	clearInterval(interval);
-	interval = setInterval(onFadeIn, 10, '.box')
+	interval = setInterval(onFadeOut, 10, 'box')
 }
 
 function onHide() {
 	clearInterval(interval);
 	interval = setInterval(onFadeOut, 10, '.box')
 }
-
 function onMove() {
 	clearInterval(interval);
-	interval = setInterval(onMoving, 10, '.box2')
+	interval - setInterval(onMoving, 10, '.box2' )
 }
 
 /***************** 이벤트등록 ******************/
-document.querySelector('.bt-show').addEventListener('click', onShow);
-document.querySelector('.bt-hide').addEventListener('click', onHide);
-document.querySelector('.bt-move').addEventListener('click', onMove);
-
+document.querySelector('.bt-show').addEventListener('click',onShow);
+document.querySelector('.bt-hide').addEventListener('click',onHide);
+document.querySelector('.bt-move').addEventListener('click',onMove);
