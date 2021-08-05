68d56f08a05d25905b54cfcb2064f9ba4648086c
1binibin <namwonbin@gmail.com>
2021-08-05 11:59:31 +0900

    animate 연습

--- js/03.show.js
+++ js/03.show.js
@@ -1,76 +1,112 @@
-$('.bt1').click(function() {
-	// 시간을 안주면 바로 없어짐(display: none;)
-	// 시간을 주면 주어진 시간동안 width, height, opacity가 줄어들면서 width, height, opacity가 0이 되면 사라짐(display: none)
-	$('.box').stop().hide(5000, function() {
-		// callback함수
-		alert('사라졌어요~');
-	}); 
-});
+// 사라져 버튼
+$('.bt1').click(function () {
+	$('.box').hide(250);
+	$('.bt1').hide();
+	$('.bt2').show();
+	$('.bt3').hide();
+	$('.bt4').show();
+	$('.bt5').hide();
+	$('.bt6').show();
 
-$('.bt2').on('click', function() {
-	$('.box').stop().show(1000); 
-});
 
-$('.bt3').on('click', function() {
-	$('.box').stop().fadeOut('slow'); // "slow"(600), "normal"(400), "fast"(200), ms
-});
+})
 
-$('.bt4').on('click', function() {
-	$('.box').stop().fadeIn('fast'); 
-});
+//나타나 버튼
+$('.bt2').click(function() {
+	$('.box').show(250);
+	$('.bt2').hide();
+	$('.bt1').show();
+	$('.bt3').show();
+	$('.bt4').hide();
+	$('.bt6').hide();
+	$('.bt5').show();
 
-$('.bt5').on('click', function() {
-	$('.box').stop().slideUp(1000); 
-});
+})
 
-$('.bt6').on('click', function() {
-	$('.box').stop().slideDown(1000); 
-});
+// 서서히 사라져 버튼
+$('.bt3').click( function () {
+	$('.box').stop().fadeOut(250);
+	$('.bt3').hide();
+	$('.bt4').show();
+	$('.bt1').hide();
+	$('.bt2').show();
+	$('.bt5').hide();
+	$('.bt6').show();
+})
 
+//서서히 나타나
+$('.bt4').click(function() {
+	$('.box').fadeIn(250);
+	$('.bt4').hide();
+	$('.bt3').show();
+	$('.bt2').hide();
+	$('.bt1').show();
+	$('.bt6').hide();
+	$('.bt5').show();
+})
+
+//위로 사라져
+$('.bt5').click( function() {
+	$('.box').slideUp(250);
+	$('.bt5').hide();
+	$('.bt6').show();
+	$('.bt1').hide();
+	$('.bt2').show();
+	$('.bt3').hide();
+	$('.bt4').show();
+})
+
+//아래로 나타나
+$('.bt6').click(function() {
+	$('.box').slideDown(250);
+	$('.bt6').hide();
+	$('.bt5').show();
+	$('.bt2').hide();
+	$('.bt1').show();
+	$('.bt4').hide();
+	$('.bt3').show();
+})
+
+// 연결 애니메이션.
 $('.bt-ani').click(function() {
-	$('.box').stop().fadeOut(1000, function() {
-		$(this).stop().fadeIn(1000, function() {
-			$(this).stop().slideUp(1000, function() {
-				$(this).stop().slideDown(1000, function() {
-					alert('애니메이션 종료');
+	$('.box').stop().hide(500, function () {
+		$('.box').stop().show(500, function () {
+			$('.box').stop().fadeOut(500, function() {
+				$('.box').stop().fadeIn(500, function() {
+					$('.box').stop().slideUp(500, function() {
+						$('.box').stop().slideDown(500, function() {
+							$('.box').stop().show(1000, function() {
+								alert('애니메이션이 종료되었습니다.')
+							})
+						})
+					})
 				})
 			})
 		})
 	})
-});
-
-$('.header-wrapper .navi').on('mouseenter', function() {
-	// $(this).children('.sub-wp').stop().slideDown(1000); // 자식선택자
-	$(this).find('.sub-wp').stop().slideDown(200); // 자손선택자
-})
-$('.header-wrapper .navi').on('mouseleave', function() {
-	$(this).find('.sub-wp').stop().slideUp(200);
 })
 
-// console.log( document.getElementById('h1').innerHTML += "마바사아" );
-
+//bt7 움직여 오른쪽으로 갔다가 왼쪽으로 되돌아오기.
 $('.bt7').click(function() {
-	//$('.box2').stop().animate(json형태의css, [ms시간(400), ('swing'(Def), 'linear'), fn(){} ] );
-	$('.box2').stop().animate({"left":"1000px"}, 2000, "swing");
-	$('.box3').stop().animate({"left":"1000px"}, 2000, "linear", function() {
-		alert('ani end')
-	});
+	$('.box2').stop().animate( {"right":"500px"}, 500,'linear', function() {
+		$('.box2').stop().animate( {"left":"25px"},1000,'swing' );
+	} )
 });
 
+//bt8 오른쪽으로 갔다가 커지고 작아지고
 $('.bt8').click(function() {
-	$('.box4').stop().animate( {
-		"width":"500px", 
-		"height":"500px", 
-		"opacity":.5,
-		"left":"400px",
-		"borderWidth":"30px"
-	}, 2000 );
-})
-// https://www.w3schools.com/jquery/eff_animate.asp
-
-$('.bt9').click(function() {
-	$('box5').addClass('active');
-})
-
-
-// console.log(JSON.parse(JSON.stringify({ name: "홍길동" })));
+	$('.box3').stop().animate( {"right": "250px"},500, 'swing', function() {
+		$(this).stop().animate( 
+			{"width":"150px",
+			"height":"150px"
+		}, 500, 'linear', function() {
+			$(this).stop().animate( 
+				{"width":"100px",
+				"height":"100px",
+				"left":"25px"
+				}, 1000, 'linear', function () {
+					$('.box3').clearQueue();
+				});
+		})
+	} )
+});
