4cf25662cb7721ea5414f77a1e7619760b4be2d0
1binibin <namwonbin@gmail.com>
2021-08-05 17:58:39 +0900

    한번더 체크 마무리

--- js/06.gallery.js
+++ js/06.gallery.js
@@ -14,16 +14,16 @@ for(var i=0; i<foods.length;i++ ) {
 	html += '</li>';
 	$('.list-wrap').append(html);
 }
-/* //1차버전
+/*//1차버전
 $('.list-wrap .thumb').click(function() {
 	$('.stage-wrap .big').attr('src', $(this).attr('src'));
 	$('.stage-wrap .name').html($(this).attr('alt'));
 	$('.list').removeClass('active');
 	$(this).parent().addClass('active');
-}) */
-
-//2차버전  대상을 appenTo 해서 뒤로 오게끔하고(position:absolute, z-index:2)적용 됨, 떠오른뒤 기존 사진 hide()실행
-// 먼저 사진의 src를 뒤에 붙인 src로 바꾸어준다. 그리고 this삭제
+})
+*/
+//2차버전  대상을 appenTo 해서 뒤로 오게끔하고(css에 position:absolute, z-index:2)적용 되어있음, 떠오른뒤 기존 사진 hide()실행
+// 먼저 사진의 src를 뒤에 붙인 src로 바꾸어준다. 그리고 this삭제 fadeIn 효과
 $('.list-wrap .thumb').click(function() {
 	var html = '<div class="hover"><img src="'+$(this).attr('src')+'" class="w100"></div>';
 	$(html).appendTo('.stage-wrap').stop().hide().fadeIn(1000, function() {
