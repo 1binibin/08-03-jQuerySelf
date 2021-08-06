49c51f3c3f1d4129c3a9c7796c244c8489e9fa8f
1binibin <namwonbin@gmail.com>
2021-08-06 11:36:46 +0900

    form 복습

--- js/12.form.js
+++ js/12.form.js
@@ -7,33 +7,28 @@ var scores = [
 
 /*************** Function init ****************/
 function init() {
-	//for(var i=0; i<scores.length; i++) {createRow(scores[i]);}
-	for(var i=0 in scores) { createRow(scores[i]) };
-	//for(var v of scores) { createRow(v)} //IE11 미지원
-	//scores.forEach(function(v, i) { createRow(v)})
+	//for(var i=0; i<scores.length; i++){ createRow(scores[i])}
+	for(var i in scores){ createRow(scores[i]) } // for ~ in 배열의 갯수만큼 돌면서 index 값을 준다.
+	//for(var v of scores){ createRow(v); } // scroes의 값을 받음 IE11 지원 X
+	//scores.forEach(function (v, i) {createRow(v)})
 }
 
 function average() {
-	for(var i=0, sum=0; i<arguments.length; i++){
+	for(var i = 0, sum= 0; i< arguments.length; i++) {
 		sum += Number(arguments[i]);
 	}
 	return (sum/arguments.length).toFixed(2);
 }
 
-
-function total() { // 가변인자
-	for(var i=0, sum=0; i<arguments.length; i++){
+function total() {
+	for(var i = 0, sum= 0; i< arguments.length; i++) {
 		sum += Number(arguments[i]);
 	}
 	return sum;
 }
 
-function createTag() {
-	
-}
-
 function createRow(data) {
-	var id = $('.score-tbody tr').length + 1;
+	var id = $('.score-tbody tr').length +1
 	var name = data.name;
 	var kor = data.kor;
 	var eng = data.eng;
@@ -48,44 +43,48 @@ function createRow(data) {
 	html += '<td>'+math+'</td>';
 	html += '<td>'+tot+'</td>';
 	html += '<td>'+avg+'</td>';
-	html += '</td>'
+	html += '</tr>';
 	$(html).prependTo('.score-tbody');
 }
 
+
 /*************** Event callback ****************/
 function onShowForm() {
-	$('.score-form').stop().slideDown(300, function () {
+	$('.score-form').stop().slideDown(300, function() {
 		document.scoreForm.name.focus();
 	});
 }
-function onSubmit() { // 검증
+
+function onSubmit() {
 	var f = document.scoreForm;
 	var name = f.name.value.trim();
 	var kor = f.kor.value;
 	var eng = f.eng.value;
 	var math = f.math.value;
-	if(name.length < 2 ){
+	if(name.length < 2){
 		alert('이름을 입력하세요.')
 		f.name.focus();
 		return false;
 	}
-	if (kor < 0 ||kor >100) {
+	if(kor < 0 || kor > 100) {
 		alert('점수가 올바르지 않습니다.')
 		f.kor.focus();
-		return false;
+		return false
 	}
-	if (eng < 0 ||eng >100) {
+	if(eng < 0 || eng > 100) {
 		alert('점수가 올바르지 않습니다.')
 		f.eng.focus();
-		return false;
+		return false
 	}
-	if (math < 0 ||math >100) {
+	if(math < 0 || math > 100) {
 		alert('점수가 올바르지 않습니다.')
 		f.math.focus();
-		return false;
+		return false
 	}
-	
+	return true;
 }
+
+
 /*************** Event init ****************/
 $('.bt-show').click(onShowForm);
 
