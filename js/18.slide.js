a72bc46d9d30bce81684e72e91e1146118dcd3ba
1binibin <namwonbin@gmail.com>
2021-08-10 23:01:33 +0900

    18.slide.js - pager성공

--- js/18.slide.js
+++ js/18.slide.js
@@ -1,61 +1,46 @@
 /*************** global init **************/
-var $wrapper = $('.wrapper1');
-var $slide = $wrapper.find('.slide-wrap');
-var $pager = $wrapper.find('.pager');
-var $btPrev = $wrapper.find('.bt-prev');
-var $btNext = $wrapper.find('.bt-next');
-var interval;       // setInterval을 넣어놓을 변수
-var idx = 0;        // animation이 움직일 값의 인자 0, -100%, -200%........
-var gap = 2000;     // setInterval의 간격
-var speed = 500;    // animation speed
-var last = $slide.find('.slide').length - 1;    // $('.slide')의 마지막 index
-
+var $slide = $('.wrapper1 .slide-wrap');
+var $pager = $('.wrapper1 .pager');
+var interval, n = 1, speed = 2000, aniSpeed = 500, cnt = $slide.find('.slide').length;
 /************** user function *************/
-function ani() {
-    $slide.stop().animate({ 'left': -(idx * 100)+'%' }, speed);
+function chgPager(idx) {
     $pager.removeClass('active');
-    $pager.eq(idx === last ? 0 : idx).addClass('active');
+    $pager.eq(idx).addClass('active');
 }
 
+
 /************** event callback ************/
+function onAni() {
+    $slide.stop().animate({'left': -n * 100 +'%'}, aniSpeed, function() {
+        if(n === cnt - 1) {
+            $slide.css('left', 0);
+            chgPager(0);
+            n = 1;
+        }
+        else {
+            chgPager(n++);
+        }
+    });
+}
+
 function onEnter() {
     clearInterval(interval);
 }
 
 function onLeave() {
-    interval = setInterval(onNext, gap);
+    interval = setInterval(onAni, speed);
 }
 
-function onPager() {
-    idx = $(this).index();
-    ani();
+function onPagerClick() {
+    n = $(this).index();
+    onAni();
 }
-
-function onPrev() {
-    //idx = (idx === 0) ? last -1 : idx - 1;
-    if( idx === 0 ) { // 0번째 사진에서 btPrev를 클릭하면 'left'에서 - last(6*100+'%')로 이동해라. 아니면 idx= last-idx
-        $slide.css('left', - last * 100+'%');
-        idx = last;
-    }
-    idx--;
-    ani();
-}
-
-function onNext() {
-    if(idx === last){ // idx가 마지막 사진(6번째)에 있으면. ('left',0) $slide의 첫번째 사진으로 가라. 아니면 idx= idx+0
-        $slide.css('left', 0);
-        idx = 0;
-    }
-    idx++;
-    ani();
-}
-
 /*************** event init ***************/
-$wrapper.mouseenter(onEnter).mouseleave(onLeave);
-$pager.click(onPager);
-$btPrev.click(onPrev);
-$btNext.click(onNext);
+interval = setInterval(onAni, 2000);
+$slide.mouseenter(onEnter).mouseleave(onLeave);
+
+$pager.click(onPagerClick)
 
 /*************** start init ***************/
-interval = setInterval(onNext, gap);
+
 
